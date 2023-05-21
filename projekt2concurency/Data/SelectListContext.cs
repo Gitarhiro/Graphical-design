56c09af82d1e5c1a2ac1fd2ed4bae521afe8d046
Szymon Bedeniczuk <sbeden@onet.pl>
2023-05-21 20:38:25 +0200

    it has working concurency for lab2

---
+++ projekt2concurency/Data/SelectListContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Warehouse.Models;
+
+namespace Warehouse.Data
+{
+    public class SelectListContext: DbContext
+    {
+        public SelectListContext(DbContextOptions<SelectListContext> options) : base()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite(@"Data Source =C:\Users\sbede\Desktop\mvc\Warehouse\products.db");
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WareHouseEntry>()
+                .Property(p => p.version)
+                .IsConcurrencyToken();
+        }
+        public virtual DbSet<WareHouseEntry> products { get; set;}
+
+    }
+}
